b3277695fa6a76aae832846f8e0087b93cf420b8
kapi819 <fuwayuri18@icloud.com>
2024-07-08 21:01:33 +0900

    fix:ログイン時におけるテストコードの修正#160

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -9,12 +9,3 @@ Capybara.register_driver :remote_chrome do |app|
   options.add_argument('window-size=1680,1050')
   Capybara::Selenium::Driver.new(app, browser: :remote, url: "http://chrome:4444/wd/hub", capabilities: options)
 end
-
-Capybara.javascript_driver = :remote_chrome
-
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :remote_chrome
-    Capybara.ignore_hidden_elements = false
-  end
-end
