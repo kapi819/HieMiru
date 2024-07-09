300621e515aab9936faefa9c676e6cac64642bce
kapi819 <fuwayuri18@icloud.com>
2024-07-09 12:20:25 +0900

    fix:ログアウト時におけるテストコードの修正#160

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -5,8 +5,8 @@ OmniAuth.config.test_mode = true
 # Line用のモック
 # '/auth/provider/callback'にリダイレクトされた時に渡されるデータを生成
 OmniAuth.config.mock_auth[:line] = OmniAuth::AuthHash.new(
-  provider: 'line', 
-  uid: '12345', 
+  provider: 'line',
+  uid: '12345',
   info: { email: 'test1@example.com', name: 'test_user' },
   credentials: { token: '1234qwer' }
 )
