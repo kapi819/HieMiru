9266965bd84723d3d075cdde0b6a0ce540b9b3aa
kapi819 <fuwayuri18@icloud.com>
2024-07-18 13:55:30 +0900

    add:冷え性に応じた解決策を追加#128

--- app/models/cold_symptom.rb
+++ app/models/cold_symptom.rb
@@ -12,7 +12,7 @@ class ColdSymptom < ApplicationRecord
   validates :symptom_body, presence: true
   validates :solution, presence: true
   validates :image_url, presence: true
-  validates :symptom_type, presence: true, numericality: { only_integer: true }
+  validates :symptom_type, inclusion: { in: %w[peripheral lower internal systemic] }, presence: true
   enum symptom_type: { peripheral: 0, lower: 1, internal: 2, systemic: 3 }
 
   def localized_symptom_type
